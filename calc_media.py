2f7866a6eca6779db410f3e893eec688b93e683e
Drika <peixedri@hotmail.com>
2021-08-18 10:58:49 -0300

    delete text

--- calc_media.py
+++
@@ -1,6 +0,0 @@
-n1=float(input("primeira nota:"))
-n2=float(input("segunda nota:"))
-n3=float(input("terceira nota:"))
-n4=float(input("quarta nota:"))
-media= (n1+n2+n3+n4)/4
-print("A media e "+str(float(media)))
